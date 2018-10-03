fa6fa693aecf85959887d35298a7a9ccf19c3bec
giiom <giiom@LOPEZ-PC>
2018-10-03 17:55:07 -0500

    ejercicios ciclos while for do while java basicos

--- src/introduccion2/Ciclos/EjerciciosCiclos.java
+++ src/introduccion2/Ciclos/EjerciciosCiclos.java
@@ -107,15 +107,15 @@ public class EjerciciosCiclos {
         /* For
         1. Mostrar la tabla de multiplicar desde el 1 hasta el 10, para un numero leido desde teclado*/
         
-        Integer numero3;
-        
-        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que quiere  de la tabla de multiplicar hasta el 10"));
-        
-        for (int i = 0; i < 11; i++) {
-            JOptionPane.showMessageDialog(null, numero3+ " x " +i+ " = " +numero3*i);
-        }
-    }
-}  
+//        Integer numero3;
+//        
+//        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que quiere  de la tabla de multiplicar hasta el 10"));
+//        
+//        for (int i = 0; i < 11; i++) {
+//            JOptionPane.showMessageDialog(null, numero3+ " x " +i+ " = " +numero3*i);
+//        }
+    
+  
         /* 2. */
 
         
@@ -126,5 +126,61 @@ public class EjerciciosCiclos {
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------*/ 
         
         /*Do While
-        1. */
+        1. Hacer un programa que sume 5 precios  de camisas en dolares, y que
+        luego muestre el total de la venta en pesos*/
+        
+//        Integer contador5, suma, precio, pesos;
+//        
+//        contador5 = 0;
+//        suma = 0;
+//        
+//        JOptionPane.showMessageDialog(null, "Suma de 5 precios de camisas en dolares");
+//        
+//        do {
+//            contador5++;
+//            
+//            precio = Integer.parseInt(JOptionPane.showInputDialog("Camisa #"+contador5+ "\n Precio de camisa en dolares:"));
+//            suma = suma+precio;
+//            
+//        }
+//        while (contador5<5); 
+//        pesos = suma * 3000;
+//        JOptionPane.showMessageDialog(null, "Precio de camisas en dolares: $" +suma+ "\n Equivalencia de peso a dolar: $1 USD = $3000 COP \n Precio de camisas convertidos a pesos: $" +pesos);
+//        
+//        }
 
+        /* 2. Se pretende leer todos los empleados de una empresa situados en un archivo y a la
+        terminacion de la lectura del archivo se debe visualizar un mensaje que muestre 
+        el numero de trabajadores que ganan mas de 2.500.000*/
+        
+//        Integer contador6, NumEmpleados2, Dinero, EmpMasDinero;
+//        
+//        contador6 = 0;
+//        EmpMasDinero = 0;
+//        
+//        NumEmpleados2 = Integer.parseInt(JOptionPane.showInputDialog("Numero de trabajadores de la empresa "));
+//        
+//        do {
+//            contador6++;
+//            Dinero = Integer.parseInt(JOptionPane.showInputDialog("Trabajador #" +contador6+ "\n Dinero que gana"));
+//            
+//            if (Dinero>2500000) {
+//                EmpMasDinero++;
+//            }
+//        } 
+//        while (contador6<NumEmpleados2);
+//        
+//        JOptionPane.showMessageDialog(null, "De " +NumEmpleados2+ " trabajadores, " +EmpMasDinero+ " ganan mas de 2.500.000");
+//        }
+        
+        /* 3. Leidos 200 numeros de teclado, se desea conocer cuales son multiplos de
+        2, de 3 y de 5*/
+        
+        Integer contador, numero, MultiplosCinco, MultiplosTres, MultiplosDos;
+        
+        
+        
+        /* 4. */
+}
+    
+    
