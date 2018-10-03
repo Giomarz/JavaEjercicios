7ee40e34edf19b14e1fe7d5ede2377b262df745e
giiom <giiom@LOPEZ-PC>
2018-10-03 18:28:13 -0500

    operaciones 2 java ejemplos

--- src/introduccion2/Ciclos/EjerciciosCiclos.java
+++ src/introduccion2/Ciclos/EjerciciosCiclos.java
@@ -18,102 +18,102 @@ public class EjerciciosCiclos {
          y contar el numero total de aprobados (una asignatura se aprueba con una nota mayor
         o igual a 3)*/
         
-        Integer contador, calificacion, NumeroEst, aprobados, noAprobados;
-        
-        contador = 0;
-        aprobados = 0;
-        noAprobados = 0;
-        
-        NumeroEst = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de estudiantes en la clase de informatica"));
-        
-        while  (contador<NumeroEst) {
-            contador++;
-            calificacion = Integer.parseInt(JOptionPane.showInputDialog (null, "Calificacion de estudiante #" +contador));
-            
-            if (calificacion>=3) {
-                aprobados++;
-            } else {
-                noAprobados++;
-            }
-            
-           
-        }
-        
-         JOptionPane.showMessageDialog(null, "De un total de " +NumeroEst+ " estudiantes, " +aprobados+ " aprobaron la materia y " +noAprobados+ " no aprobaron");
+//        Integer contador, calificacion, NumeroEst, aprobados, noAprobados;
+//        
+//        contador = 0;
+//        aprobados = 0;
+//        noAprobados = 0;
+//        
+//        NumeroEst = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de estudiantes en la clase de informatica"));
+//        
+//        while  (contador<NumeroEst) {
+//            contador++;
+//            calificacion = Integer.parseInt(JOptionPane.showInputDialog (null, "Calificacion de estudiante #" +contador));
+//            
+//            if (calificacion>=3) {
+//                aprobados++;
+//            } else {
+//                noAprobados++;
+//            }
+//            
+//           
+//        }
+//        
+//         JOptionPane.showMessageDialog(null, "De un total de " +NumeroEst+ " estudiantes, " +aprobados+ " aprobaron la materia y " +noAprobados+ " no aprobaron");
          
         /*2. Elaborar un algoritmo que permita ingresar 20 numeros y muestre todos los numeros menores o iguales a 25*/
         
-        Integer contador2, numero, menores;
-        
-        contador2 = 0;
-        menores = 0;
-        
-        while (contador2<19) {
-            contador2++;
-            numero = Integer.parseInt(JOptionPane.showInputDialog("Numero #" +contador2));
-            
-            if (numero<=25) {
-                menores++;
-            }
-        }
-        
-        JOptionPane.showMessageDialog(null, "de " +contador2+ " numeros ingresados, " +menores+ " son menores o iguales a 25");
+//        Integer contador2, numero, menores;
+//        
+//        contador2 = 0;
+//        menores = 0;
+//        
+//        while (contador2<19) {
+//            contador2++;
+//            numero = Integer.parseInt(JOptionPane.showInputDialog("Numero #" +contador2));
+//            
+//            if (numero<=25) {
+//                menores++;
+//            }
+//        }
+//        
+//        JOptionPane.showMessageDialog(null, "de " +contador2+ " numeros ingresados, " +menores+ " son menores o iguales a 25");
         
         /*3. elaborar un algoritmo que permita el ingreso de 100 numeros el algoritmo debe sumar todos los numeros que sean multiplos de 3*/
         
-        Integer contador3, numero2, multiplos, SumaMultiplos;
-        
-        contador3 = 0;  
-        multiplos = 0;
-        SumaMultiplos = 0;
-        
-        while (contador3<99) {
-            contador3++;
-            numero2 = Integer.parseInt(JOptionPane.showInputDialog("Numero #" +contador3));
-            
-            if (numero2%3==0) {
-                multiplos++;
-                SumaMultiplos = SumaMultiplos + numero2;
-            } else {
-                JOptionPane.showMessageDialog(null, "No es multiplo de 3, no suma");
-            }
-        }
-        
-        JOptionPane.showMessageDialog(null, "Suma de numeros multiplos de 3: " +SumaMultiplos);
+//        Integer contador3, numero2, multiplos, SumaMultiplos;
+//        
+//        contador3 = 0;  
+//        multiplos = 0;
+//        SumaMultiplos = 0;
+//        
+//        while (contador3<99) {
+//            contador3++;
+//            numero2 = Integer.parseInt(JOptionPane.showInputDialog("Numero #" +contador3));
+//            
+//            if (numero2%3==0) {
+//                multiplos++;
+//                SumaMultiplos = SumaMultiplos + numero2;
+//            } else {
+//                JOptionPane.showMessageDialog(null, "No es multiplo de 3, no suma");
+//            }
+//        }
+//        
+//        JOptionPane.showMessageDialog(null, "Suma de numeros multiplos de 3: " +SumaMultiplos);
         
         /*4. se pretende leer las edades de todos los empleados de una empresa a la terminacion de la lectura 
         se debe visualizar un mensaje que muestre el numero de trabajadores mayores a 65 años*/
         
-        Integer contador4, NumEmpleados, edad, EmpMayores;
-        
-        contador4 = 0;
-        EmpMayores = 0;
-        
-        NumEmpleados = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de empleados de la empresa"));
-        
-        while (contador4<NumEmpleados) {
-            contador4++;
-            edad = Integer.parseInt(JOptionPane.showInputDialog("Edad del empleado #" +contador4));
-            
-            if (edad>=65) {
-                EmpMayores++;
-            }
-        }
-        
-        JOptionPane.showMessageDialog(null, "Numero de empleados que son mayores: " +EmpMayores);
+//        Integer contador4, NumEmpleados, edad, EmpMayores;
+//        
+//        contador4 = 0;
+//        EmpMayores = 0;
+//        
+//        NumEmpleados = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de empleados de la empresa"));
+//        
+//        while (contador4<NumEmpleados) {
+//            contador4++;
+//            edad = Integer.parseInt(JOptionPane.showInputDialog("Edad del empleado #" +contador4));
+//            
+//            if (edad>=65) {
+//                EmpMayores++;
+//            }
+//        }
+//        
+//        JOptionPane.showMessageDialog(null, "Numero de empleados que son mayores: " +EmpMayores);
 
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------*/ 
         
         /* For
         1. Mostrar la tabla de multiplicar desde el 1 hasta el 10, para un numero leido desde teclado*/
         
-        Integer numero3;
-        
-        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que quiere  de la tabla de multiplicar hasta el 10"));
-        
-        for (int i = 0; i < 11; i++) {
-            JOptionPane.showMessageDialog(null, numero3+ " x " +i+ " = " +numero3*i);
-        }
+//        Integer numero3;
+//        
+//        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que quiere  de la tabla de multiplicar hasta el 10"));
+//        
+//        for (int i = 0; i < 11; i++) {
+//            JOptionPane.showMessageDialog(null, numero3+ " x " +i+ " = " +numero3*i);
+//        }
     
   
         /* 2. */
@@ -129,58 +129,60 @@ public class EjerciciosCiclos {
         1. Hacer un programa que sume 5 precios  de camisas en dolares, y que
         luego muestre el total de la venta en pesos*/
         
-        Integer contador5, suma, precio, pesos;
-        
-        contador5 = 0;
-        suma = 0;
-        
-        JOptionPane.showMessageDialog(null, "Suma de 5 precios de camisas en dolares");
-        
-        do {
-            contador5++;
-            
-            precio = Integer.parseInt(JOptionPane.showInputDialog("Camisa #"+contador5+ "\n Precio de camisa en dolares:"));
-            suma = suma+precio;
-            
-        }
-        while (contador5<5); 
-        pesos = suma * 3000;
-        JOptionPane.showMessageDialog(null, "Precio de camisas en dolares: $" +suma+ "\n Equivalencia de peso a dolar: $1 USD = $3000 COP \n Precio de camisas convertidos a pesos: $" +pesos);
-        
-        }
+//        Integer contador5, suma, precio, pesos;
+//        
+//        contador5 = 0;
+//        suma = 0;
+//        
+//        JOptionPane.showMessageDialog(null, "Suma de 5 precios de camisas en dolares");
+//        
+//        do {
+//            contador5++;
+//            
+//            precio = Integer.parseInt(JOptionPane.showInputDialog("Camisa #"+contador5+ "\n Precio de camisa en dolares:"));
+//            suma = suma+precio;
+//            
+//        }
+//        while (contador5<5); 
+//        pesos = suma * 3000;
+//        JOptionPane.showMessageDialog(null, "Precio de camisas en dolares: $" +suma+ "\n Equivalencia de peso a dolar: $1 USD = $3000 COP \n Precio de camisas convertidos a pesos: $" +pesos);
+//        
+//        }
 
         /* 2. Se pretende leer todos los empleados de una empresa situados en un archivo y a la
         terminacion de la lectura del archivo se debe visualizar un mensaje que muestre 
         el numero de trabajadores que ganan mas de 2.500.000*/
         
-        Integer contador6, NumEmpleados2, Dinero, EmpMasDinero;
+//        Integer contador6, NumEmpleados2, Dinero, EmpMasDinero;
         
-        contador6 = 0;
-        EmpMasDinero = 0;
-        
-        NumEmpleados2 = Integer.parseInt(JOptionPane.showInputDialog("Numero de trabajadores de la empresa "));
-        
-        do {
-            contador6++;
-            Dinero = Integer.parseInt(JOptionPane.showInputDialog("Trabajador #" +contador6+ "\n Dinero que gana"));
-            
-            if (Dinero>2500000) {
-                EmpMasDinero++;
-            }
-        } 
-        while (contador6<NumEmpleados2);
-        
-        JOptionPane.showMessageDialog(null, "De " +NumEmpleados2+ " trabajadores, " +EmpMasDinero+ " ganan mas de 2.500.000");
-        }
+//        contador6 = 0;
+//        EmpMasDinero = 0;
+//        
+//        NumEmpleados2 = Integer.parseInt(JOptionPane.showInputDialog("Numero de trabajadores de la empresa "));
+//        
+//        do {
+//            contador6++;
+//            Dinero = Integer.parseInt(JOptionPane.showInputDialog("Trabajador #" +contador6+ "\n Dinero que gana"));
+//            
+//            if (Dinero>2500000) {
+//                EmpMasDinero++;
+//            }
+//        } 
+//        while (contador6<NumEmpleados2);
+//        
+//        JOptionPane.showMessageDialog(null, "De " +NumEmpleados2+ " trabajadores, " +EmpMasDinero+ " ganan mas de 2.500.000");
+//        }
         
         /* 3. Leidos 200 numeros de teclado, se desea conocer cuales son multiplos de
         2, de 3 y de 5*/
         
-        Integer contador, numero, MultiplosCinco, MultiplosTres, MultiplosDos;
+        Integer contador7, numero4, MultiplosCinco, MultiplosTres, MultiplosDos;
         
-        
-        
+        contador7 = 0;
+        MultiplosCinco = 0;
+        MultiplosTres= 0;
+        MultiplosDos= 0;
         /* 4. */
-}   }
+    }
     
     
