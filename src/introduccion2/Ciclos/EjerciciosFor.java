f2e7890bf14f24649bba5b39419ba3fb5666301a
giiom <giiom@LOPEZ-PC>
2018-10-04 11:28:18 -0500

    ejercicios for java terminados!

--- src/introduccion2/Ciclos/EjerciciosFor.java
+++ src/introduccion2/Ciclos/EjerciciosFor.java
@@ -48,8 +48,39 @@ public class EjerciciosFor {
         JOptionPane.showMessageDialog(null, "De los " +i+ " numeros leidos, \n " +multiplocinco+ " son multiplos de cinco, \n" +multiplotres+ ""
         + " son multiplos de tres, \n" +multiplodos+ " son multiplos de 2");
         
-        /* 3. Realizaar un algoritmo que muestre por pantalla la tabla de multiplicar decreciente del 2*/
+        /* 3. Realizar un algoritmo que muestre por pantalla la tabla de multiplicar decreciente del 2*/
+        
+        Integer numero3;     
+        
+        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
+     
+        for (int i2 = 10; i2 > 0; i2--) {
 
-        /* 4. */
+         JOptionPane.showMessageDialog(null, numero3+ " x " +i2+ " = " +numero3*i2);
+      }
+
+        /* 4. hacer un programa  que sume los numeros  que son multipos de 3,5 y de 7  comprendidos  entre los numeros 100 y 300 inclusive */
+       
+       Integer i3, numero4, multiplotres1, multiplocinco1, multiplosiete1;
+       
+       multiplotres = 0;
+       multiplocinco1 = 0;
+       multiplosiete1 = 0;
+       multiplotres1 = 0;
+       
+       for (i3 = 100; i3 < 300; i3++) {
+           numero4 = Integer.parseInt(JOptionPane.showInputDialog("Numero #" +i3));
+           
+           if (numero4%3==0) {
+               multiplotres1++;
+           } else if (numero4%5==0) {
+               multiplocinco1++;
+           } else if (numero4%7==0) {
+               multiplosiete1++;
+           }
+       }
+       
+       JOptionPane.showMessageDialog(null, multiplosiete1 +" son multiplos de siete"+ " \n " +multiplotres+ " son multiplos de tres, \n" +multiplocinco+ ""
+               + " son multiplos de cinco, \n");
     }
 }
